eebb22ec2df5e15866feb54006b16436cd14ba65
farnoodlotfali <farnoodlotfali@gmail.com>
2026-01-04 21:45:55 +0330

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
+    path: "/",
     localPatterns: [
       {
         pathname: "/cars/**",
